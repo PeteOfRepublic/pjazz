759a0b3298674bee47c26ec7c0a8f1cd2288b496
Pete Haughie <pete.haughie@sourcefabric.org>
2016-06-07 20:41:02 +0100

    Work so far loads the link by href

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,61 @@
 window.onload = function(){
-  console.log("ping");
+
+  var linkList = document.getElementById( 'links' ),
+      pjazz = linkList.getElementsByTagName( 'a' ),
+      xhr = new XMLHttpRequest();
+      contentElem = document.getElementById( 'content' );
+
+  for (var i = 0; i < pjazz.length; i++ ) {
+    pjazz[i].onclick = function( event ) {
+      event.preventDefault();
+      var pjlink = this.href,
+          getProt = window.location.protocol,
+          getHost = window.location.host,
+          getPort = window.location.port;
+
+      pjlink = pjlink.replace( getProt + "/", "" )
+                     .replace( getHost + "/", "" )
+                     .replace( getPort, "" );
+
+      xhr.open( 'GET', pjlink, true );
+      xhr.send( null );
+
+      xhr.onreadystatechange = function() {
+        var state = xhr.readyState,
+            status = xhr.status;
+
+        if ( state == 4 && status == 200 ){
+          // console.log( "success", xhr );
+        } else if (
+          state == 0 |
+          state == 1 |
+          state == 2 |
+          state == 3
+        ) {
+          // ignore these states
+        } else if ( status == 301 ) {
+          console.log( "This content has been permamently moved" );
+        } else if ( status == 302 ) {
+          console.log( "The server is sending a 302 http message but probably means a 303 and it has been temporarily moved" );
+        } else if ( status == 308 ) {
+          console.log( "This content has been permamently redirected" );
+        } else if ( status == 400 ) {
+          console.log( "Bad Request" );
+        } else if ( status == 401 ) {
+          console.log( "You are not authorised to see this content" );
+        } else if ( status == 403 ) {
+          console.log( "You are forbidden from seeing this content" );
+        } else if ( status == 404 ) {
+          console.log( "This content was not found" );
+        } else if ( status == 500 ) {
+          console.log( "There was a problem with the server" );
+        } else {
+          // if it doesn't fit in any of the above then print out the state and status
+          console.log( "readystate = ", state, "server status = ", state );
+        }
+      }
+
+    }
+  }
+
 }
